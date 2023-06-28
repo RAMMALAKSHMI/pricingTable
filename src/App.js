d4c4b38f2196ca806140b92e54eeaa94c6b8bfd1
RAMMALAKSHMI <rammalakshmi2012@gmail.com>
2023-06-29 00:47:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container py-5'>
+      <div className='row'>
+        <div className='col-lg-4'>
+        <div className='card mb-5 card-lg-0'>
+          <div className='card-body'>            
+              <h5 className='card-title text-muted text-center'>FREE</h5>
+              <h1 className='card-price text-center'>$0<span className='month'> /month</span></h1>
+              <hr/>
+              <ul className='list-group fa-ul'>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Single User</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>5GB Storage</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Unlimited Public Projects</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Unlimited Private Projects</span></li>
+                <li className='list-group-item text-muted'><span><i className="fas fa-times mr-2"></i>Dedicated Phone Support</span></li>
+                <li className='list-group-item text-muted'><span><i className="fas fa-times mr-2"></i>Community Access</span></li>
+                <li className='list-group-item text-muted'><span><i className="fas fa-times mr-2"></i>Free Subdomain</span></li>
+                <li className='list-group-item text-muted'><span><i className="fas fa-times mr-2"></i>Monthly Status Reports</span></li>
+
+              </ul>
+              <button type="button" className="btn btn-primary btn-lg btn-block">Enroll</button>
+
+            </div>
+          </div>
+        </div>
+
+        <div className='col-lg-4'>
+        <div className='card mb-5 card-lg-0'>
+          <div className='card-body'>            
+              <h5 className='card-title text-muted text-center'>PLUS</h5>
+              <h1 className='card-price text-center'>$9<span className='month'> /month</span></h1>
+              <hr/>
+              <ul className='list-group fa-ul'>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i><strong>5 User</strong></span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>50GB Storage</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Unlimited Public Projects</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Community Access</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Unlimited Private Projects</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Dedicated Phone Support</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Free Subdomain</span></li>
+                <li className='list-group-item text-muted'><span><i className="fas fa-times mr-2"></i>Monthly Status Reports</span></li>
+              </ul>
+              <button type="button" className="btn btn-primary btn-lg btn-block">Enroll</button>
+
+            </div>
+          </div>
+        </div>
+
+        <div className='col-lg-4'>
+        <div className='card mb-5 card-lg-0'>
+          <div className='card-body'>            
+              <h5 className='card-title text-muted text-center'>PRO</h5>
+              <h1 className='card-price text-center'>$49<span className='month'> /month</span></h1>
+              <hr/>
+              <ul className='list-group fa-ul'>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i><strong>Unlimited Users</strong></span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>150GB Storage</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Unlimited Public Projects</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Community Access</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Unlimited Private Projects</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Dedicated Phone Support</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i><strong>Unlimited </strong>Free Subdomain</span></li>
+                <li className='list-group-item'><span><i className="fas fa-check mr-2"></i>Monthly Status Reports</span></li>
+
+              </ul>
+              <button type="button" className="btn btn-primary btn-lg btn-block">Enroll</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
